5e6a8b0335ef9f6502160dc6e48af7d15fdc70f2
hannah-cobb <hannah.e.cobb@gmail.com>
2014-11-19 22:55:16 -0800

    Game Class
    Initiated variables and functions for the game class. Also I’m using
    the “hello world” folder, rather than the other one, because I know how
    to get that one to work… Hopefully the xCode - Visual Studio transition
    won’t be too problematic.

--- helloWorld/helloWorld/main.cpp
+++ helloWorld/helloWorld/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
-int main(){
-cout << "Hello world!" << endl;
-//fdhlskahgjkhsfdsal;hfejkljkdfs
+class game{
+private:
+    string name;
+    int wins;
+    int losses;
+    int timesPlayed;
 
+public:
+    string getName(){
+        return name;
+    }
+    int getWins(){
+        return wins;
+    }
+    int getLosses(){
+        return losses;
+    }
+    double getAverage(){
+        return wins / (wins + losses);
+    }
+    int getTimesPlayed(){
+        return timesPlayed;
+    }
+    void setName(string _name){
+        name = _name;
+    }
+    
+    
+};
+
+int main(){
+    
 }
