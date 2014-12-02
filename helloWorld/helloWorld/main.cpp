e1299f2f58b7dcde090713f84dc62397d914fe18
hannah-cobb <hannah.e.cobb@gmail.com>
2014-12-02 14:07:07 -0800

    cout nonsense
    stuff with cout and commenting garbage code and making the thing
    actually compile, even though it is compiling badly and currently
    Hannah always wins

--- helloWorld/helloWorld/main.cpp
+++ helloWorld/helloWorld/main.cpp
@@ -1,37 +1,35 @@
 //#define __APPLE__ // Comment this line out when compiling on a Windows platform
-
-#ifdef __APPLE__
-#include "CoreFoundation/CoreFoundation.h"
-#endif
+//
+//#ifdef __APPLE__
+//#include "CoreFoundation/CoreFoundation.h"
+//#endif
 
 
 #include <iostream>
 #include <string>
 #include "game.h"
 #include "player.h"
-//#include "game.cpp"
-#include "player.h"
 
 using namespace std;
 
 int main()
 {
-    // ----------------------------------------------------------------------------
-    // This makes relative paths work in C++ in Xcode by changing directory to the Resources folder inside the .app bundle
-	#ifdef __APPLE__
-    CFBundleRef mainBundle = CFBundleGetMainBundle();
-    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
-    char path[PATH_MAX];
-    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
-    {
-        // error!
-    }
-    CFRelease(resourcesURL);
-    
-    chdir(path);
-    std::cout << "Current Path: " << path << std::endl;
-    #endif
-    // ----------------------------------------------------------------------------
+//    // ----------------------------------------------------------------------------
+//    // This makes relative paths work in C++ in Xcode by changing directory to the Resources folder inside the .app bundle
+//	#ifdef __APPLE__
+//    CFBundleRef mainBundle = CFBundleGetMainBundle();
+//    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+//    char path[PATH_MAX];
+//    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
+//    {
+//        // error!
+//    }
+//    CFRelease(resourcesURL);
+//    
+//    chdir(path);
+//    std::cout << "Current Path: " << path << std::endl;
+//    #endif
+//    // ----------------------------------------------------------------------------
     
     char replay = 'y';
     while (replay == 'y' || replay == 'Y')
