ada3774a335f3c7289db1083b42def11b11d5e85
mmoffett <marissamoffett@gmail.com>
2014-12-07 19:09:20 -0800

    Comments in player.cpp
    added function comments for functions in player.cpp

--- helloWorld/helloWorld/player.cpp
+++ helloWorld/helloWorld/player.cpp
@@ -1,7 +1,11 @@
 
 #include "player.h"
 
-
+/*
+* A constructor for the player class.
+*
+* @param  _name		the player's name
+*/
 player::player(string _name)
 {
 	name = _name;
@@ -9,6 +13,15 @@ player::player(string _name)
 	losses = 0;
 	timesPlayed = 0;
 }
+
+/*
+* A constructor for the player class.
+*
+* @param  _name			the player's name
+* @param  _wins			the number of wins player has
+* @param _losses		the number of losses the player has
+* @param _timesPlayed	the number of timesPlayed
+*/
 player::player(string _name, int _wins, int _losses, int _timesPlayed)
 {
 	name = _name;
@@ -16,18 +29,42 @@ player::player(string _name, int _wins, int _losses, int _timesPlayed)
 	losses = _losses;
 	timesPlayed = _timesPlayed;
 }
+
+/*
+* Returns the player's name.
+*
+* @return     the name
+*/
     string player::getName()
     {
         return name;
     }
+
+/*
+* Returns how many wins the player has.
+*
+* @return     the number of wins
+*/
     int player::getWins()
     {
         return wins;
     }
+
+/*
+* Returns how many losses the player has.
+*
+* @return     the number of losses
+*/
     int player::getLosses()
     {
         return losses;
     }
+
+/*
+* Returns the player's average.
+*
+* @return     the average
+*/
     double player::getAverage()
     {
 		double gamesWon = wins + losses;
@@ -36,34 +73,76 @@ player::player(string _name, int _wins, int _losses, int _timesPlayed)
 		else
 			return 0;
     }
+
+/*
+* Returns how many times the player has played.
+*
+* @return     the number of games played
+*/
     int player::getTimesPlayed()
     {
         return timesPlayed;
     }
+
+/*
+* Set's the players name.
+*
+* @param	_name     the player's name
+*/
     void player::setName(string _name)
     {
         name = _name;
     }
+
+/*
+* Increases the player's number of wins.
+*/
 	void player::upWins()
 	{
 		wins++;
 	}
+
+/*
+* Increases the player's number of losses.
+*/
 	void player::upLosses()
 	{
 		losses++;
 	}
+
+/*
+* Increases the player's number of games played.
+*/
 	void player::upTimesPlayed()
 	{
 		timesPlayed++;
 	}
+
+/*
+* Sets the player's win count.
+* 
+* @param  win	the number of wins
+*/
     void player::setWinCount(int win)
     {
         wins = win;
     }
+
+/*
+* Sets the player's loss count.
+* 
+* @param  loss	the number of losses
+*/
     void player::setLoss(int loss)
     {
         losses = loss;
     }
+
+/*
+* Sets the player's games played.
+* 
+* @param  played	the games played
+*/
     void player::setTimesPlayed(int played)
     {
         timesPlayed = played;
